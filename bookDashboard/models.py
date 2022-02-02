6f263df62e8ce2bdd24c4d97ee80a0c7dcd67952
Daniel Krzeminski <Dankrzeminski32@gmail.com>
2022-02-01 22:09:23 -0600

    db booklist migrations

--- bookDashboard/models.py
+++ bookDashboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class bookList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookList')
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class book(models.Model):
+    bookList = models.ForeignKey(bookList, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=100)
+
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
