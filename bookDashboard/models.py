84325a5524d65ff857211af15338686e76a79c7e
Daniel Krzeminski <Dankrzeminski32@gmail.com>
2022-02-18 08:17:27 -0600

    bootstrap cards

--- bookDashboard/models.py
+++ bookDashboard/models.py
@@ -3,9 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class book(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
     title = models.CharField(max_length=100)
-
     complete = models.BooleanField()
 
     def __str__(self):
