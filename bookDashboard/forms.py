31e57da331495e73a5660dcc3c01440f2a3af7ba
Daniel Krzeminski <Dankrzeminski32@gmail.com>
2022-02-21 13:47:21 -0600

    image upload working

--- bookDashboard/forms.py
+++ bookDashboard/forms.py
@@ -5,11 +5,13 @@ from bookDashboard.models import Book
 class newBookForm(ModelForm):
     def __init__(self, **kwargs):
         self.user = kwargs.pop('user', None)
+        self.image = kwargs.pop('image', None)
         super(newBookForm, self).__init__(**kwargs)
     
     def save(self, commit=True):
         obj = super(newBookForm,self).save(commit=False)
         obj.user = self.user
+        obj.image = self.image
         if commit:
             obj.save()
         return obj
--- bookDashboard/views.py
+++ bookDashboard/views.py
@@ -11,7 +11,7 @@ def userView(request):
     print(Book.objects.filter(user__id=request.user.id))
     context = {}
     if request.method == "POST":
-        form = newBookForm(data=request.POST, user=request.user)
+        form = newBookForm(data=request.POST, user=request.user, image=request.FILES.get('image','default.jpg'))
         if form.is_valid():
             form.save()
             return redirect('dashboardView')
